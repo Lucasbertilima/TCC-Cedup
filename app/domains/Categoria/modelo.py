4e6fc4dcd44ecac7e1f93d54f1d5da96c0568ec0
Lucasbertilima <57449627+Lucasbertilima@users.noreply.github.com>
2021-08-30 10:10:22 -0300

    Delete app/domains/Categoria directory

--- app/domains/Categoria/modelo.py
+++
@@ -1,8 +0,0 @@
-from database import db    
-
-
-class Categoria(db.Model):
-    __tablename__ = 'categoria'
-
-    id = db.Column(db.String(36), default=lambda: str(uuid4()), primary_key=True)
-    nome = db.Column(db.String(55), nullable=False)
